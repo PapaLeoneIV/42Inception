13dfb2973d0245945a1662b91ca506fae29aea31
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-09-11 12:27:34 +0200

    [next js] fully functional

---
+++ srcs/bonus/nextjs/tools/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# Function to print messages with colors
+print_info() {
+    echo -e "\e[34m[INFO]\e[0m $1"
+}
+
+print_success() {
+    echo -e "\e[32m[SUCCESS]\e[0m $1"
+}
+
+print_error() {
+    echo -e "\e[31m[ERROR]\e[0m $1"
+}
+
+
